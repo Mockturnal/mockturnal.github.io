36fac1b995423b5c3efd530b6a5a2ed7a638fef0
HotPotatoC <HotPotatoC@users.noreply.github.com>
2021-07-05 09:30:37 +0000

    deploy: StudioKarsa/studiokarsa-web@ddc23d9b8faf951c6a7a432d5c93c312302b6dba

--- component---src-pages-404-jsx-e0570fba0cdc8794da5e.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[256],{1657:function(t,e,n){"use strict";n.r(e);var u=n(7294),a=n(775),l=n(1277);e.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-jsx-e0570fba0cdc8794da5e.js.map
